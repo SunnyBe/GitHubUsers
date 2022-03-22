d899b0effeb6ba8b1169bf6883c3dd159b5364a1
Sunday Ndu <ndusunday@gmail.com>
2022-03-22 13:37:39 +0100

    Setup buildSrc for later use, added dependencies to gradle build files, Setup basic file structure

--- app/src/main/java/com/sundayndu/githubusers/presentation/MainActivity.kt
+++ app/src/main/java/com/sundayndu/githubusers/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.sundayndu.githubusers
+package com.sundayndu.githubusers.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sundayndu.githubusers.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/sundayndu/githubusers/presentation/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.sundayndu.githubusers.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class MainViewModel @Inject constructor(
+
+): ViewModel() {
+}
